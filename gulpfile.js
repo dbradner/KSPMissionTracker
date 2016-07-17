26f38324f3d9d3bc0fd4b2a1cfad750ab172fd70
mrmagician8787 <dbradner0607066@gmail.com>
2016-07-16 22:46:20 -0400

    Committing before writing del statements in gulp. Updates to gulp, placeholders on the views, storagemanager and start of repos implemented, models implemented.

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,56 @@
 var gulp = require('gulp');
-// var gulpLoadPlugins = require('gulp-load-plugins');
-// var plugins = gulpLoadPlugins();
+
+var autoprefixer = require('gulp-autoprefixer');
+var cleancss = require('gulp-clean-css');
+var concat = require('gulp-concat');
+var del = require('del');
 var tslint = require('gulp-tslint');
+var typescript = require('gulp-typescript');
+var uglify = require('gulp-uglify');
 var util = require('gulp-util');
 
+
+// ## THINGS TO RUN FOR ALL BUILDS ## //
+
 gulp.task('ts-lint', function(){
-    return gulp.src("src/**/*.ts")
+    return gulp.src(["src/**/*.ts", "public/scripts/*.ts"])
         .pipe(tslint({
             formatter: 'prose'
         }))
         .pipe(tslint.report({
-            emitError: false
+            emitError: false,
+            summarizeFailureOutput: true
         }))
 });
 
-gulp.task('build', ['ts-lint'], function(){
+
+// ## THINGS TO RUN FOR DEV BUILDS ## //
+
+
+
+
+// ## THINGS TO RUN FOR PROD BUILDS ## //
+
+gulp.task('clean-prod', function () {
+    
+});
+
+gulp.task('build-js-prod', ['ts-lint', 'clean-prod'], function () {
+    return gulp.src(["src/**/*.ts"])
+        .pipe(typescript({
+            removeComments: true
+        }))
+        .pipe(concat('KSPMT.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/prod'));
+});
+
+gulp.task('build-all-prod', ['ts-lint'], function(){
     util.log(util.colors.green("All build tasks finished. Build successful!"));
 });
+
+
+// TODO: Probably going to point to dev build...
+// gulp.task('default', ['build-all'], function(){
+//
+// });
